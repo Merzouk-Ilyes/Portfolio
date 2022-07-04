4da25eb390668728f1a0a17a1ee2e54d57021460
Merzouk-ilyes <i.merzouk@esi-sba.dz>
2022-07-04 19:59:54 +0100

    ++phone

--- components/socials.js
+++ components/socials.js
@@ -6,24 +6,15 @@ import {
   AiFillGithub,
   AiFillLinkedin,
 } from "react-icons/ai";
-import {FaFacebookF} from "react-icons/fa"
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { Tooltip,Tag } from "@chakra-ui/react";
+import { FaFacebookF } from "react-icons/fa";
 function Socials() {
   return (
-    <div className="grid grid-cols-6 gap-2 w-[370px]  relative  z-10 dark:text-white text-[27px] text-gray-600 my-8  mx-[5%]  md:mx-[10%] lg:mx-[20%] ">
-      <a
-        href="mailto:merzouk.ilyase@gmail.com"  
-        className=" w-fit"
-      >
+    <div className="grid grid-cols-7 gap-2 w-[370px]  relative  z-10 dark:text-white text-[27px] text-gray-600 my-8  mx-[5%]  md:mx-[10%] lg:mx-[20%] ">
+      <a href="mailto:merzouk.ilyase@gmail.com" className=" w-fit">
         {" "}
         <MdEmail />
-      </a>
-      <a
-        href="https://www.instagram.com/ilyes17_mk/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className=" w-fit"
-      >
-        <AiFillInstagram />{" "}
       </a>
       <a
         href="https://www.facebook.com/ilyas.merzouk.90"
@@ -34,6 +25,14 @@ function Socials() {
         <FaFacebookF />{" "}
       </a>
       <a
+        href="https://www.instagram.com/ilyes17_mk/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="w-fit"
+      >
+        <AiFillInstagram />{" "}
+      </a>
+      <a
         href="https://www.linkedin.com/in/merzouk-ilyes-reda-5154a41b6/"
         target="_blank"
         rel="noopener noreferrer"
@@ -42,7 +41,7 @@ function Socials() {
         {" "}
         <AiFillLinkedin />{" "}
       </a>
-      <a
+      {/* <a
         href="https://twitter.com/IlyesReda17"
         target="_blank"
         rel="noopener noreferrer"
@@ -50,7 +49,7 @@ function Socials() {
       >
         {" "}
         <AiOutlineTwitter />
-      </a>
+      </a> */}
       <a
         href="https://github.com/Merzouk-Ilyes"
         target="_blank"
@@ -60,8 +59,26 @@ function Socials() {
         {" "}
         <AiFillGithub />
       </a>
+      <div className="w-fit">
+        <Tooltip
+          label="+213 558 44 95 41"
+          fontSize="large"
+          // className="w-fit"
+          // placement="auto"
+        >
+          <CustomCard ></CustomCard>
+        </Tooltip>
+      </div>
     </div>
   );
 }
 
 export default Socials;
+
+const CustomCard = React.forwardRef(({ children, ...rest }, ref) => (
+  <Tag ref={ref} {...rest}>
+    <BsFillTelephoneFill className="text-[22px]" />
+    </Tag>
+));
+
+CustomCard.displayName = "CustomCard"
