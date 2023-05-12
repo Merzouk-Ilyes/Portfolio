072a49fefb2c416029caf0e169317a342b054b49
Merzouk-ilyes <i.merzouk@esi-sba.dz>
2023-05-12 11:29:52 +0200

    2023 Updates V1

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,7 @@ import {
 } from "../public/sass/card.module.sass";
 import { MdAlternateEmail } from "react-icons/md";
 import { AiOutlineInstagram } from "react-icons/ai";
-import { FiTwitter, FiGithub, FiLinkedin,FiFacebook } from "react-icons/fi";
+import { FiTwitter, FiGithub, FiLinkedin, FiFacebook } from "react-icons/fi";
 function About() {
   return (
     <Layout>
@@ -27,37 +27,35 @@ function About() {
            leading-[25px] tracking-wide	
            text-[15px] md:text-[17px]  w-[380px]  relative z-40  md:w-[550px]  "
             >
-              Hello! My name is Merzouk Ilyes Reda i&apos;m a software engineer from Algeria, My interest in web development
-              started back in 2018 when i created my first  website and since then i kept exploring the vast world of web
-              development and specifically the  software development lifecycle including analysis, development, testing and deployment.
-              currently I&apos;m interested in DevOps, testing & AI.
-              
+              Hello! My name is Merzouk Ilyes Reda i&apos;m a software engineer
+              from Algeria, My interest in web development started back in 2018
+              when i created my first website and since then i kept exploring
+              the vast world of web development and specifically the software
+              development lifecycle including analysis, development, testing and
+              deployment. currently I&apos;m interested in DevOps, testing & AI.
               <br />
-           
               <br />
               <span>
                 {" "}
-                Here are some of  the technologies that i&apos;m proficient at :
+                Here are some of the technologies that i&apos;m proficient at :
                 <br />
                 <br />
                 <div className="grid grid-cols-2">
-                <ul>
-                  <li>	&#916; Javascript ES6+ </li>
-                  <li>	&#916; React Js - Redux</li>
-                  <li>	&#916; Next Js</li>
-                  <li>	&#916; Node Js</li>
-                  <li>	&#916; Typescript </li>
-                  <li>	&#916; Angular 13+ </li>
-                 
-                </ul>
-                <ul>
-                  <li>	&#916; PostgreSql </li>
-                  <li>	&#916; MongoDB </li>
-                  <li>	&#916; Spring Boot </li>
-                  <li>	&#916; Docker </li>
-                  <li>	&#916; Git </li>
-                </ul>
-              
+                  <ul>
+                    <li> &#916; Javascript ES6+ </li>
+                    <li> &#916; React Js - Redux</li>
+                    <li> &#916; Next Js</li>
+                    <li> &#916; Node Js</li>
+                    <li> &#916; Typescript </li>
+                    <li> &#916; Angular 13+ </li>
+                  </ul>
+                  <ul>
+                    <li> &#916; PostgreSql </li>
+                    <li> &#916; MongoDB </li>
+                    <li> &#916; Spring Boot </li>
+                    <li> &#916; Docker </li>
+                    <li> &#916; Git </li>
+                  </ul>
                 </div>
               </span>
             </p>
@@ -102,7 +100,7 @@ function About() {
                 objectFit="contain"
                 alt="image"
               />
-                {/* <Image
+              {/* <Image
                   src="/images/logos/fire.png"
                   width="100%"
                   height="100%"
@@ -127,13 +125,13 @@ function About() {
                 alt="image"
               />
               <Image
-              src="/images/logos/PG.png"
-              width="100%"
-              height="100%"
-              layout="responsive"
-              objectFit="contain"
-              alt="image"
-            />
+                src="/images/logos/PG.png"
+                width="100%"
+                height="100%"
+                layout="responsive"
+                objectFit="contain"
+                alt="image"
+              />
               <Image
                 src="/images/logos/db.png"
                 width="100%"
@@ -159,7 +157,7 @@ function About() {
                   {" "}
                   <MdAlternateEmail />
                 </a>
-                
+
                 <a
                   href="https://www.linkedin.com/in/merzouk-ilyes-reda/"
                   target="_blank"
@@ -169,7 +167,7 @@ function About() {
                   {" "}
                   <FiLinkedin />{" "}
                 </a>
-               
+
                 <a
                   href="https://github.com/Merzouk-Ilyes"
                   target="_blank"
